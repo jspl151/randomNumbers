dcbc650c666659fcd34ccb2c2c35f1af49558ca5
jspl151 <jayasutha151@gmail.com>
2023-06-03 05:19:43 +0000

    renamed.

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 const hex = '0123456789abcdef';
-const hexLength= hex.length;
+const hexLength = hex.length;
 
-const getRandomNos = (min,max)=> Math.floor(Math.random() * (max - min) + min);
+const getRandomNo = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
 
 const getRandomCharacter = () => {
-  const charLength= chars.length ;
+  const charLength = chars.length;
   let result = '';
-  result = chars.charAt(getRandomNos(1,charLength+1));
-  
+  result = chars.charAt(getRandomNo(1, charLength + 1));
+
   return result;
 }
 
 const getRandomString = (length) => {
-  let RandomString = '';
-  for( i=0;i < length;i++ )
-  RandomString += getRandomCharacter(); 
-  
-  return RandomString;
+  let randomString = '';
+  for (i = 0; i < length; i++)
+    randomString += getRandomCharacter();
+
+  return randomString;
 }
 
 const getRandomHex = (length) => {
-  let RandomHex = '';
-  for( i=0;i < length;i++ )
-  RandomHex += hex.charAt(getRandomNos(1,hexLength+1));
-  
-  return RandomHex;
+  let randomHex = '';
+  for (i = 0; i < length; i++)
+    randomHex += hex.charAt(getRandomNo(1, hexLength + 1));
+
+  return randomHex;
 }
 
-const getUuid = (length) => {
-  let RandomHex = '';
-  for( i=0;i < length;i++ )
-  RandomHex += getRandomHex(length);
-  
-  return RandomHex;
+const getUUID = (length) => {
+  let randomHex = '';
+  for (i = 0; i < length; i++)
+    randomHex += getRandomHex(length);
+
+  return randomHex;
 }
 
 const main = () => {
-  console.log(getRandomNos(0,15));
+  console.log(getRandomNo(0, 15));
   console.log(getRandomCharacter());
   console.log(getRandomString(4));
   console.log(getRandomHex(4));
-  const uuidLength = [8,4,4,4,12] ;
-  const uuid = uuidLength.map(getUuid);
+  const uuidLength = [8, 4, 4, 4, 12];
+  const uuid = uuidLength.map(getUUID);
   console.log(uuid.join('-'));
-  
+
 };
 
 main();
