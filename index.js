30599b1606aab4b3b29448c1da5cebbf364c6b56
jspl151 <jayasutha151@gmail.com>
2023-06-03 05:10:32 +0000

    uuid created.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
+const hex = '0123456789abcdef';
+const hexLength= hex.length;
+
 const getRandomNos = (min,max)=> Math.floor(Math.random() * (max - min) + min);
 
 
@@ -12,19 +15,38 @@ const getRandomCharacter = () => {
 }
 
 const getRandomString = (length) => {
-  const charLength= chars.length ;
   let RandomString = '';
   for( i=0;i < length;i++ )
   RandomString += getRandomCharacter(); 
   
   return RandomString;
 }
+
+const getRandomHex = (length) => {
+  let RandomHex = '';
+  for( i=0;i < length;i++ )
+  RandomHex += hex.charAt(getRandomNos(1,hexLength+1));
   
+  return RandomHex;
+}
+
+const getUuid = (length) => {
+  let RandomHex = '';
+  for( i=0;i < length;i++ )
+  RandomHex += getRandomHex(length);
+  
+  return RandomHex;
+}
 
 const main = () => {
-  console.log(getRandomNos(0,9));
+  console.log(getRandomNos(0,15));
   console.log(getRandomCharacter());
   console.log(getRandomString(4));
+  console.log(getRandomHex(4));
+  const uuidLength = [8,4,4,4,12] ;
+  const uuid = uuidLength.map(getUuid);
+  console.log(uuid.join('-'));
+  
 };
 
 main();
